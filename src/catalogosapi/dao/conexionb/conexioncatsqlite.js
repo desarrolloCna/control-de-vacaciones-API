3c5eee991533472a999461430cf3387dce0d9824
JulioCurruchiche <juliocurruchiche96@gmail.com>
2026-08-20 09:09:24 -0600

    fix: revert catalogos db connection to fix vercel crash

--- src/catalogosapi/dao/conexionb/conexioncatsqlite.js
+++ src/catalogosapi/dao/conexionb/conexioncatsqlite.js
@@ -1,8 +1,7 @@
 import { createClient } from "@libsql/client";
-import "dotenv/config";
 
 // Crear cliente de conexión a SQLite usando @libsql/client
 export const Connection = createClient({
-    url: process.env.DB_CATALOGOS_URL,
-    authToken: process.env.DB_TURSO_AUTH_TOKEN
+    url: "libsql://catalogosbd-desarrollocna.aws-us-east-1.turso.io",
+    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NjMwOTkwOTgsImlkIjoiYzFhN2UzMmUtYmUxNC00ZWIwLTlkZTEtMjc5ZTljMTU1MTg1IiwicmlkIjoiODA1Nzg1ZjItM2NjZS00MjY1LTk1NTMtNmUwZjFmODZjY2ViIn0.51Wc06l-KFG22S3gMsBfq2_6MyFqEno89BL0fEsnoNvnC__loPjpi7VxTFLn3aP4UI3fFX9w59pT4ewa8rUUCQ"
 });
